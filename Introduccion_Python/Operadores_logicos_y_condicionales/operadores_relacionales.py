87db580a32e53c3bb123fc9a72b5644f2a73aef3
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-01-29 21:52:06 -0500

    Aprendiendo el uso de los tipos de datos booleanos y los operadores relacionales

---
+++ Introduccion_Python/Operadores_logicos_y_condicionales/operadores_relacionales.py
@@ -0,0 +1,40 @@
+"""
+Tipos Boleanos
+"""
+valor_verdadero = True
+valor_falso = False
+
+print(valor_verdadero, "Conversión a entero: ", int(valor_verdadero))
+print(valor_falso, "Conversión a entero: ", int(valor_falso))
+
+"""
+Operadores Relacionales
+"""
+
+valor_a = 20
+valor_b = 18
+
+operador_mayor = valor_a > valor_b
+operador_menor = valor_b < valor_a
+operador_mayor_igual = valor_a >= valor_b
+operador_menor_igual = valor_b <= valor_a
+operador_distonto = valor_a != valor_b
+operador_igual = valor_a == valor_b
+
+print(operador_mayor)
+print(operador_menor)
+print(operador_mayor_igual)
+print(operador_menor_igual)
+print(operador_distonto)
+print(operador_igual)
+
+
+valor_c = 5
+valor_d = 15
+valor_e = 25
+
+comprobacion_valores = (valor_c + valor_d) * valor_c > valor_a ** 2
+print(comprobacion_valores)
+
+
+
