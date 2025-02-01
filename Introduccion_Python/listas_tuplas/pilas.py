0d988af0de3c536ab224f436ebc35746e2492981
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-01-31 19:43:50 -0500

    Raelizando un ejercicio sobre el uso de pilas en python

---
+++ Introduccion_Python/listas_tuplas/pilas.py
@@ -0,0 +1,17 @@
+"""
+Pilas 
+"""
+
+pila = []
+
+pila.append(1)
+pila.append(2)
+pila.append(3)
+pila.append(4)
+
+print(pila)
+
+# elimina el ultimo elemento de la pila
+elemento = pila.pop()
+
+print(pila)
