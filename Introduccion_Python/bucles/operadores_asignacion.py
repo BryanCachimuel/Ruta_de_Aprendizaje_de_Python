11844fbaee60c45a9d80f1c0faec323b05d21fae
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-01-31 20:50:46 -0500

    Estudiando los bucles en python, pero antes de ello se realizo un ejercicio sobre los operadores de asignación

---
+++ Introduccion_Python/bucles/operadores_asignacion.py
@@ -0,0 +1,64 @@
+"""
+Operadores de Asignación
+"""
+
+# operador de asignación = 
+a = 10
+a = a + 5
+print(a)
+
+# forma simplificada con el operador  += -> suma en asignación
+b = 125
+b += 10
+print(b)
+
+# operador  -= -> resta en asignación
+c = 47
+c -= 2
+print(c)
+
+# operador  *= -> multiplicación en asignación
+d = 20
+d *= 4
+print(d)
+
+# operador  /= -> división en asignación
+e = 144
+e /= 4
+print(e)
+
+# operador %= -> modulo en asignación
+f = 15
+f %= 2
+print(f)
+
+# operador **= -> potencia en asignación
+g = 8
+g **= 3
+print(g)
+
+# incremento y decremento no existe en python
+# h = 25
+# print(h++)
+# print(h--)
+
+# lo que se puede hacer para aplicar ese incremento y decremento es lo siguiente
+h = 25
+
+h += 1
+h += 1
+h += 1
+
+print(h)
+
+i = 32
+
+i -= 1
+i -= 1
+i -= 1
+i -= 1
+i -= 1
+i -= 1
+
+print(i)
+
