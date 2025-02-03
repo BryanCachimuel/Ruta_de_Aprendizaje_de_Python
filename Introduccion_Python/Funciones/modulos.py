26dc9f05d66872c0c41183c32ae6ea57f613ef16
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-02-03 18:50:11 -0500

    Creando otra función en la cual se mande a llamar a una lista con nombres y al final se podrá imprimir esa lista como un diccionario donde se almacena los nombres y saludos aleatorios con los que se va a trabajar

--- Introduccion_Python/Funciones/modulos.py
+++ Introduccion_Python/Funciones/modulos.py
@@ -2,6 +2,7 @@
 Crear Modulos en Python
 """
 import random
+from typing import List, Dict
 
 def formato_aleatorio()->str:
     formatos = [
@@ -22,3 +23,17 @@ def nombre_saludo(nombre:str)->str:
     return saludo
 
 print(nombre_saludo("Bryan"))
+
+# función que recibira una lista de nombres y retornará un diccionario
+def nombres_saludos(nombres:List)->Dict:
+    saludos = {}
+    for nombre in nombres:
+        saludo = nombre_saludo(nombre)
+        saludos[nombre] = saludo
+
+    return saludos
+
+# creando una lista con nombres
+# nombres = ["Nelson","Cristoper","Kevin","Lucas","Richard"]
+
+# print(nombres_saludos(nombres))
