af7db94814c53c4d59627a20e1746624cc4f6487
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-02-05 21:54:16 -0500

    Aprendiendo a utilizar matrices anidadas y como acceder a los elementos de cada fila y columna de la matriz

---
+++ Introduccion_Python/listas_tuplas/matrices_listas.py
@@ -0,0 +1,22 @@
+"""
+Matrices con listas anidadas
+"""
+
+matriz = [[1,2,3],
+          [4,5,6],
+          [7,8,9]]
+
+# Imprimir la matriz para que se vea una matriz adecuadamente
+print(f"{matriz[0]} \n{matriz[1]} \n{matriz[2]}")
+
+# Mostrar el número 1
+print(f"Mostrar el número: {matriz[0][0]}")
+
+# Mostrar el número 4
+print(f"Mostrar el número: {matriz[1][0]}")
+
+# Mostrar el número 8
+print(f"Mostrar el número: {matriz[2][1]}")
+
+# Mostrar el número 3
+print(f"Mostrar el número: {matriz[0][2]}")
