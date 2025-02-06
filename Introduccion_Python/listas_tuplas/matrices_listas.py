bba4920e290e5a9390224d8a9b12a56537437bd2
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-02-05 22:07:17 -0500

    Recorriendo una matriz mediante el ciclo for e ir obteniendo sus elementos de acuerdo a lo que se requiera obtener como respuesta

--- Introduccion_Python/listas_tuplas/matrices_listas.py
+++ Introduccion_Python/listas_tuplas/matrices_listas.py
@@ -20,3 +20,23 @@ print(f"Mostrar el número: {matriz[2][1]}")
 
 # Mostrar el número 3
 print(f"Mostrar el número: {matriz[0][2]}")
+
+# recorrido de la matriz mediante el ciclo for
+matrix = [[4,8,12],
+          [16,20,24],
+          [28,32,36]]
+
+print("Mostrar todos los elementos de la matriz fila por fila")
+for row in matrix:
+    print(row)
+
+print("Mostrar todos los elementos de la matriz elemento a elemento en columna")
+for row in matrix:
+    for element in row:
+        print(element)
+
+print("Mostrar todos los elementos de una matriz en formato de matriz")
+for row in matrix:
+    for element in row:
+        print(element, end=" ")
+    print()
