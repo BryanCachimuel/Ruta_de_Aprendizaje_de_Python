9d6683dfbce3c3a0a4b8e97d7e8c968d3767541e
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-02-07 22:09:41 -0500

    Agregando la barra de menus para que sea visible en el frame

--- Proyecto-Tkinder/catalogo-peliculas/catalogo-peliculas/catalogo_peliculas.py
+++ Proyecto-Tkinder/catalogo-peliculas/catalogo-peliculas/catalogo_peliculas.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from client.gui_app import Frame
+from client.gui_app import Frame, barra_menu
 
 """
 Creación de la venta
@@ -9,6 +9,7 @@ def main():
     root.title('Catalogo de Peliculas')
     root.iconbitmap('img/cp-logo.ico')
     root.resizable(0,0)                # renderizado de la ventana
+    barra_menu(root)
 
     # Importanto del frame
     app = Frame(root=root)
