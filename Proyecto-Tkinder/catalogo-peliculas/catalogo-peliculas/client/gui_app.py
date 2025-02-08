b2eb9eb23cfb489a8902dc5f71bbe11a03f5def6
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-02-07 21:18:39 -0500

    Agregando un barra de Menus con el siguiente contenido: Inicio con 3 submenus, Consultas, Configuración y Ayuda

--- Proyecto-Tkinder/catalogo-peliculas/catalogo-peliculas/client/gui_app.py
+++ Proyecto-Tkinder/catalogo-peliculas/catalogo-peliculas/client/gui_app.py
@@ -1,5 +1,22 @@
 import tkinter as tk
 
+def barra_menu(root):
+    barra_menu = tk.Menu(root)
+    root.config(menu=barra_menu, width=300, height=300)
+
+    # Inicio
+    menu_inicio = tk.Menu(barra_menu, tearoff=0)
+    barra_menu.add_cascade(label='Inicio', menu=menu_inicio)
+
+    # submenus de inicio
+    menu_inicio.add_command(label='Crear Registro en BD')
+    menu_inicio.add_command(label='Eliminar Registro en BD')
+    menu_inicio.add_command(label='Salir', command=root.destroy)
+
+    barra_menu.add_cascade(label='Consultas')
+    barra_menu.add_cascade(label='Configuración')
+    barra_menu.add_cascade(label='Ayuda')
+
 class Frame(tk.Frame):
     def __init__(self, root = None):
         super().__init__(root, width=480, height=320)
