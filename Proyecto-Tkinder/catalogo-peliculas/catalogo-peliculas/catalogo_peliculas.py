2192dabc8e79248fea981383e535171a5f55763f
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-02-07 21:03:27 -0500

    Importando las configuraciones realizadas del frame hacia la función main del proyecto

--- Proyecto-Tkinder/catalogo-peliculas/catalogo-peliculas/catalogo_peliculas.py
+++ Proyecto-Tkinder/catalogo-peliculas/catalogo-peliculas/catalogo_peliculas.py
@@ -1,11 +1,19 @@
 import tkinter as tk
+from client.gui_app import Frame
 
+"""
+Creación de la venta
+"""
 def main():
     root = tk.Tk()
     root.title('Catalogo de Peliculas')
     root.iconbitmap('img/cp-logo.ico')
-    root.resizable(0,0)
-    root.mainloop()
+    root.resizable(0,0)                # renderizado de la ventana
+
+    # Importanto del frame
+    app = Frame(root=root)
+
+    app.mainloop()   # todo el código de la proyecto ba antes de está linea
 
 if __name__ == "__main__":
     main()
