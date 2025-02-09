1e6f66aa187762a11b94870d3bc59060a2a38978
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-02-08 21:31:15 -0500

    Agregando la librería messagebox para poder crear alertas para cuando se crea una tabla en la base de datos y cuando se la elimina a la tabla en la base de datos

--- Proyecto-Tkinder/catalogo-peliculas/catalogo-peliculas/model/pelicula_dao.py
+++ Proyecto-Tkinder/catalogo-peliculas/catalogo-peliculas/model/pelicula_dao.py
@@ -1,4 +1,5 @@
 from .conexion_db import ConexionDB
+from tkinter import messagebox
 
 def crear_tabla():
     conexion = ConexionDB()
@@ -12,12 +13,30 @@ def crear_tabla():
         PRIMARY KEY(id_pelicula AUTOINCREMENT)
     )'''
 
-    conexion.cursor.execute(sql)
-    conexion.cerrar()
+    try:
+        conexion.cursor.execute(sql)
+        conexion.cerrar()
+
+        titulo = 'Crear Registro'
+        mensaje = 'Se creo la tabla en la base de datos'
+        messagebox.showinfo(titulo,mensaje)
+    except:
+        titulo = 'Crear Registro'
+        mensaje = 'La tabla ya está creada'
+        messagebox.showwarning(titulo,mensaje)
 
 def borrar_tabla():
     conexion = ConexionDB()
     sql = 'DROP TABLE peliculas'
 
-    conexion.cursor.execute(sql)
-    conexion.cerrar()
+    try:
+        conexion.cursor.execute(sql)
+        conexion.cerrar()
+
+        titulo = 'Borrar Registro'
+        mensaje = 'La tabla de la base de datos se borro con éxito'
+        messagebox.showinfo(titulo,mensaje)
+    except:
+        titulo = 'Borrar Registro'
+        mensaje = 'No existe tabla para borrar'
+        messagebox.showerror(titulo,mensaje)
