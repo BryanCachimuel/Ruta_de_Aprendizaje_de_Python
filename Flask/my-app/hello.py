21d15fcb6de10ef18a0850b1a9c1b9a82b12cc49
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-02-10 20:37:28 -0500

    Aplicando el uso de rutas y vistas en flask

--- Flask/my-app/hello.py
+++ Flask/my-app/hello.py
@@ -3,10 +3,16 @@ from flask import Flask
 # cuando se pone __name__ estanis diciendo que está es nuestra aplicación de flask
 app = Flask(__name__)
 
-# se crea una ruta
+# se crean las rutas
+# se puede agregar varias rutas a una vista
 @app.route('/')
+@app.route('/index')
+def index():
+    return '<h1>Página de Inicio</h1>'
+
+@app.route('/saludo')
 def saludo():
-    return 'Saludos desde Flask'
+    return '<h1>Página de Saludos</h1>'
 
 # para que los errores se puedan ver en el navegador se pone en formato debug cuando se manda a ejecutar el proyecto
 # flask --app hello --debug run 
