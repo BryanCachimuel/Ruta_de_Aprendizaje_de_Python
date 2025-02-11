5f0475228f4f60384fd93659c96f59b8fd31328a
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-02-10 21:00:09 -0500

    Aplicando el uso de variables en las rutas y como es forma de utilización

--- Flask/my-app/hello.py
+++ Flask/my-app/hello.py
@@ -14,5 +14,29 @@ def index():
 def saludo():
     return '<h1>Página de Saludos</h1>'
 
+# se puede poner variables en las rutas mediante el uso de -> <>
+@app.route('/asignando/<nombre>')
+def asignar(nombre):
+    return f'<h1>Página de asignación de nombres. ejemplo: {nombre}</h1>'
+
+# se puede poner el tipo de dato que se manda en las variables de la url
+# pueden ser de tipo: string, int, float, path, uuid
+
+@app.route('/datos/<string:nombre>/<int:edad>')
+def informacion(nombre,edad):
+    return f'<h1>Hola tu información es: nombre: {nombre} y tu edad es: {edad} años</h1>'
+
+@app.route('/mascotas')
+@app.route('/mascotas/<nombre>')
+@app.route('/mascotas/<string:nombre>/<int:edad>')
+def animales(nombre = None, edad = None):
+    if nombre == None and edad == None:
+        return '<h1>Saludos a todos los animales</h1>'
+    elif edad == None:
+        return f'<h1>Holas, {nombre}</h1>'
+    else:
+        return f'<h1>Hola mascota de nombre: {nombre}, tu doble de edad es: {edad*2} años</h1>'
+
+
 # para que los errores se puedan ver en el navegador se pone en formato debug cuando se manda a ejecutar el proyecto
 # flask --app hello --debug run 
