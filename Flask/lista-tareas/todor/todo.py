2064fccf58519a8191b4a15679e80591c193f6ce
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-02-18 21:25:45 -0500

    Creando el script para poder realizar la creación de una nueva tarea

--- Flask/lista-tareas/todor/todo.py
+++ Flask/lista-tareas/todor/todo.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, g
 
 from todor.auth import login_required
 
@@ -18,7 +18,16 @@ def index():
     todos = Todo.query.all()
     return render_template('todo/index.html', todos = todos)
 
-@bp.route('/create')
+@bp.route('/create', methods=('GET','POST'))
 @login_required
 def create():
-    return "Crear una tarea"
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+
+        todo = Todo(g.user.id, title, description)
+
+        db.session.add(todo)
+        db.session.commit()
+        return redirect(url_for('todo.index'))
+    return render_template('todo/create.html')
