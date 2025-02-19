08da5e3c3d2023c3c1a29c171fb4d6acb9b6174b
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-02-18 20:57:33 -0500

    Integrando el script en la función index para mostrar todas las tareas registradas por usuarios

--- Flask/lista-tareas/todor/todo.py
+++ Flask/lista-tareas/todor/todo.py
@@ -2,6 +2,9 @@ from flask import Blueprint, render_template
 
 from todor.auth import login_required
 
+from .models import Todo, User
+from todor import db
+
 # Blueprint permite organizar las vistas del proyecto
 
 # con el __name__ estamos mandando la configuración del modulo actual
@@ -11,7 +14,9 @@ bp = Blueprint('todo', __name__, url_prefix='/todo')
 @bp.route('/list')
 @login_required  # decorador que indica que para ingresar a esta vista se necesita iniciar sesión
 def index():
-    return render_template('todo/index.html')
+    # recuperando todas las tareas
+    todos = Todo.query.all()
+    return render_template('todo/index.html', todos = todos)
 
 @bp.route('/create')
 @login_required
