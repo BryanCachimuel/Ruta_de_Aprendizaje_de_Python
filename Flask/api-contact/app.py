6d231733298bccec5a76a86d0320e3ece51adf98
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-02-25 20:48:53 -0500

    Creando el modelo Contact con sus respectivos atributos, un método para realizar los datos y se pueda obtener a travez de un diccionario los cuales van a poder ser tratados después como objetos json

--- Flask/api-contact/app.py
+++ Flask/api-contact/app.py
@@ -4,3 +4,23 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///contacts.db"
 db = SQLAlchemy(app)
+
+# Modelo Contact para la base de datos
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    phone = db.Column(db.String(10), nullable=False)
+
+    # método para serializar los datos
+    def serializable(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email,
+            'phone': self.phone
+        }
+
+# Migrar las tablas hacia la base de datos
+with app.app_context():
+    db.create_all()
