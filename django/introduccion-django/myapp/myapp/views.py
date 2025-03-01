9d6fdaa3af8ca999b7601ea6783ed94ff1086468
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-02-28 22:18:02 -0500

    Creando una nueva simulación de vista para poder comprobar el ingreso de un usuario a la página web de acuerdo a su edad

--- django/introduccion-django/myapp/myapp/views.py
+++ django/introduccion-django/myapp/myapp/views.py
@@ -6,3 +6,11 @@ def saludar(request):
     numeros_ordenados = sorted(numeros)
     print(request.GET)
     return HttpResponse(str(numeros_ordenados))
+
+def verificar(request, nombre, edad):
+    if edad < 12:
+        mensaje = f'Hola, {nombre} perdon pero no puede ingresar a está página'
+    else:
+        mensaje = f'Hola, {nombre} si puedes ingresar a está página'
+
+    return HttpResponse(mensaje)
