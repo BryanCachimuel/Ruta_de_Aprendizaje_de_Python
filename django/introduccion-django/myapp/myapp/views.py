252b566a2b9e71ff6b39dfc7c60f3eecf550b2de
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-02-28 22:08:26 -0500

    Ordenar los valores que se ingresen como números en la url

--- django/introduccion-django/myapp/myapp/views.py
+++ django/introduccion-django/myapp/myapp/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 
 def saludar(request):
-    print(request)
+    numeros = [int(n) for n in request.GET['numeros'].split(',')]
+    print(numeros)
+    numeros_ordenados = sorted(numeros)
     print(request.GET)
-    return HttpResponse(request.GET['numeros'])
+    return HttpResponse(str(numeros_ordenados))
