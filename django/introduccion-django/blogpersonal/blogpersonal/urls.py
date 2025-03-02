6dc9f56eb9559406e2ffc020066e346257f82608
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-01 22:40:12 -0500

    Integrando con include las aplicaciones y sus url de las vistas para lo que es: core, perfil y blog

--- django/introduccion-django/blogpersonal/blogpersonal/urls.py
+++ django/introduccion-django/blogpersonal/blogpersonal/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    # Core
+    path('', include('core.urls')),
+    # Blog
+    path('blog/', include('blog.urls')),
+    # Perfil
+    path('perfil/', include('perfil.urls')),
+    # Admin
     path('admin/', admin.site.urls),
 ]
