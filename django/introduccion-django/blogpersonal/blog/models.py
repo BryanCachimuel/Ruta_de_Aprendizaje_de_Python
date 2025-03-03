b6d54a69baa20e55ea8c690cb3881281cfb37da5
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-02 21:01:05 -0500

    Creando la clase Post con los atributos correspondientes para el modelo de blog

--- django/introduccion-django/blogpersonal/blog/models.py
+++ django/introduccion-django/blogpersonal/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    image = models.ImageField()
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    content = models.TextField()
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
