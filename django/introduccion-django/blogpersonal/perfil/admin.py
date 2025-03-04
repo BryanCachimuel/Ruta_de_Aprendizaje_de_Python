76af9970db5c27226e0d5e55b238d824e2034ec2
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-03 21:30:22 -0500

    Registrando el modelo Porject en el administrador y configurando su respectivo panel

--- django/introduccion-django/blogpersonal/perfil/admin.py
+++ django/introduccion-django/blogpersonal/perfil/admin.py
@@ -4,4 +4,14 @@ from django.contrib import admin
 from .models import Project 
 
 # Register your models here.
-admin.site.register(Project)
+#admin.site.register(Project)
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'created')
+    list_editable = ('title', )
+    list_filter = ('title', 'created', 'updated')
+    search_fields = ('title',)
+
+    # para campos de solo lectura
+    readonly_fields = ('created', 'updated')
