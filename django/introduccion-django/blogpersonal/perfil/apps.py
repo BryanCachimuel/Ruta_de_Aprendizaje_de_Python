d9b54c52b9ee55c375e68ae480e88fd509a0b437
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-03 21:32:08 -0500

    Realizando el proceso de traducción de los campos de proyectos

--- django/introduccion-django/blogpersonal/perfil/apps.py
+++ django/introduccion-django/blogpersonal/perfil/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class PerfilConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'perfil'
+    # se puede traducir también la pestaña del administrador que redirige hacia los post
+    #verbose_name = 'Perfiles'
