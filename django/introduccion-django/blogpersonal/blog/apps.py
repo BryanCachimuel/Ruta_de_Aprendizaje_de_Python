d3b602c012749f2a015d2aef0b1cbac79312bc06
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-03 20:34:53 -0500

    Introduciendo la forma de traducción de los campos de cada entidad y realizando configuraciones de ordenamiento de los datos de los campos

--- django/introduccion-django/blogpersonal/blog/apps.py
+++ django/introduccion-django/blogpersonal/blog/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog'
+    # se puede traducir también la pestaña del administrador que redirige hacia los post
+    #verbose_name = 'Blogs'
--- django/introduccion-django/blogpersonal/blog/models.py
+++ django/introduccion-django/blogpersonal/blog/models.py
@@ -1,14 +1,22 @@
 from django.db import models
 
 # Create your models here.
+# verbose_name sirve para traducir los nombres de los campos para que se vean en español en el panel de administración
 class Post(models.Model):
-    image = models.ImageField()
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    content = models.TextField()
+    image = models.ImageField(verbose_name='Imagen')
+    title = models.CharField(max_length=200, verbose_name='Título')
+    description = models.TextField(verbose_name='Descripción')
+    content = models.TextField(verbose_name='Contenido')
 
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de Creación')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Fecha de Actualización')
+
+    # Traduce los subititulos de cada sección de las aplicaciones 
+    class Meta:
+        verbose_name = 'Publicación'
+        verbose_name_plural = 'Publicaciones'
+        # forma de ordenar los registros
+        ordering = ['-created']
 
     def __str__(self):
         return self.title
