02c358854a1d59e0704959b87d622f47b6fe1769
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-03 19:49:00 -0500

    Creando las funciones posts y post para los blogs

--- django/introduccion-django/blogpersonal/blog/views.py
+++ django/introduccion-django/blogpersonal/blog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, HttpResponse
 
+# Importando el modelo
+from .models import Post
+
 # Create your views here.
 def posts(request):
-    return HttpResponse('<h1>Página de Publicaciones</h1>')
+    blogs = Post.objects.all()
+    return HttpResponse(blogs)
 
 def post(request, id):
-    return HttpResponse('<h1>Página de Blog</h1>')
+    blog = Post.objects.get(id=id)
+    content = f'{blog.title} - {blog.description}'
+    return HttpResponse(content)
