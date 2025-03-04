ab1eea909565dc56b4d15a2c33028897bef5580d
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-03 20:49:04 -0500

    Configurando ckeditor para la utilización del mismo en la sección de Publicaciones de blogs

--- django/introduccion-django/blogpersonal/blog/models.py
+++ django/introduccion-django/blogpersonal/blog/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
+# importando ckeditor
+from ckeditor.fields import RichTextField
+
 # Create your models here.
 # verbose_name sirve para traducir los nombres de los campos para que se vean en español en el panel de administración
 class Post(models.Model):
     image = models.ImageField(verbose_name='Imagen')
     title = models.CharField(max_length=200, verbose_name='Título')
     description = models.TextField(verbose_name='Descripción')
-    content = models.TextField(verbose_name='Contenido')
+    content = RichTextField(verbose_name='Contenido')
 
     created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de Creación')
     updated = models.DateTimeField(auto_now=True, verbose_name='Fecha de Actualización')
