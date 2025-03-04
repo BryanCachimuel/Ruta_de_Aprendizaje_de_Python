d874eeabab06ef16ced981ad5b02c6a9ba48b09e
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-03 20:24:01 -0500

    Personalizando el panel de administración de la parte de blog para postear posts

--- django/introduccion-django/blogpersonal/blog/admin.py
+++ django/introduccion-django/blogpersonal/blog/admin.py
@@ -4,4 +4,15 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+#admin.site.register(Post)
+
+# con este decorador se va a importar directamente al administrador
+@admin.register(Post)
+class Postadmin(admin.ModelAdmin):
+    list_display = ('id', 'image', 'title', 'description', 'created')
+    list_display_links = ('id', 'title',)
+    list_filter = ('created', 'updated')
+    search_fields = ('title', 'description')
+
+    # para campos de solo lectura
+    readonly_fields = ('created', 'updated')
