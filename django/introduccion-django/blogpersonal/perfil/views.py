8498f605f304c9880c969b4daceba8a90d0a6630
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-04 21:34:06 -0500

    Renderizando la vista de profile.html y aplicando la herencia en bloques desde la vista base.html hacia la vista profile.html

--- django/introduccion-django/blogpersonal/perfil/views.py
+++ django/introduccion-django/blogpersonal/perfil/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Importando el modelo
 from .models import Project
@@ -6,4 +6,4 @@ from .models import Project
 # Create your views here.
 def profile(request):
     projects = Project.objects.all()
-    return HttpResponse(projects)
+    return render(request, 'profile.html')
