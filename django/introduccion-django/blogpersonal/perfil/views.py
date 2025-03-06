1af56d9da6584de8f107ed2d82c6bd50a90c88a4
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-05 21:11:00 -0500

    Enviando a la vista profile.html todos los registros de proyectos que se an realizado

--- django/introduccion-django/blogpersonal/perfil/views.py
+++ django/introduccion-django/blogpersonal/perfil/views.py
@@ -6,4 +6,4 @@ from .models import Project
 # Create your views here.
 def profile(request):
     projects = Project.objects.all()
-    return render(request, 'profile.html')
+    return render(request, 'profile.html', {'projects':projects})
