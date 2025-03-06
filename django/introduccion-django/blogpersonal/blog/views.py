0ba3323a0ab0a2f4405b9bc1e63752898fbd3e7f
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-05 20:33:01 -0500

    Remitiendo los datos de post para mostrarlos en la vista de blogs.html

--- django/introduccion-django/blogpersonal/blog/views.py
+++ django/introduccion-django/blogpersonal/blog/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Importando el modelo
 from .models import Post
 
 # Create your views here.
 def posts(request):
-    blogs = Post.objects.all()
-    return HttpResponse(blogs)
+    first_post = Post.objects.first()
+    posts = Post.objects.all()
+    return render(request, 'blogs.html', {'posts':posts, 'first_post': first_post})
 
 def post(request, id):
     blog = Post.objects.get(id=id)
     content = f'{blog.title} - {blog.description}'
-    return HttpResponse(content)
+    return render(request, 'blog.html')
