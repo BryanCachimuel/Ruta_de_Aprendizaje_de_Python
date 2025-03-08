5583e610c1796253ca7ae2e786682417361275f7
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-07 21:19:29 -0500

    Configuración hacia la base de datos sqlite3

--- Flask/app-flask/app.py
+++ Flask/app-flask/app.py
@@ -1,6 +1,15 @@
 from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# configuración de sqlite3
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///articulos.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
 
 @app.route('/')
 def home():
