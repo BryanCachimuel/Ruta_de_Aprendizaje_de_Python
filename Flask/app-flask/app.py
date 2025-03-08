1e45c2e248ee790bf6d2127536e7841d0017baad
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-07 21:25:23 -0500

    Creando el modelo Article que servirá como entidad de la base de datos

--- Flask/app-flask/app.py
+++ Flask/app-flask/app.py
@@ -9,6 +9,15 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+# Modelos
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+
+    # se imprime por consola el titulo del artículo
+    def __repr__(self):
+        return f'<Article: {self.title}>'
 
 
 @app.route('/')
