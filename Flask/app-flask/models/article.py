be9a32bb78a29bdc65e539eb76e2562ed73f69eb
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-08 21:00:08 -0500

    Creando el modelo de User con sus respectivos atributos

---
+++ Flask/app-flask/models/article.py
@@ -0,0 +1,13 @@
+# Modelo de Artículo
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+
+    # se imprime por consola el titulo del artículo
+    def __repr__(self):
+        return f'<Article: {self.title}>'
