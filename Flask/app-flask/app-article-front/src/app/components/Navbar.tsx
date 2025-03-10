5937c6d73cb55230fdf10725b02e291df72be3d5
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-09 21:32:10 -0500

    Creando el componente de la barra de navegación que estará incluida en el Layout

--- Flask/app-flask/app-article-front/src/app/components/Navbar.tsx
+++ Flask/app-flask/app-article-front/src/app/components/Navbar.tsx
@@ -1,1 +1,21 @@
+import React from 'react'
 
+const Navbar = () => {
+  return (
+    <nav className='bg-indigo-600 text-white p-4'>
+        <div className='container mx-auto flex justify-between items-center'>
+            <h1 className='text-xl font-bold'>Mi Artículo</h1>
+            <ul className='flex space-x-4'>
+                <li>
+                    <a href="/">Inicio</a>
+                </li>
+                <li>
+                    <a href="/">Crear</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
