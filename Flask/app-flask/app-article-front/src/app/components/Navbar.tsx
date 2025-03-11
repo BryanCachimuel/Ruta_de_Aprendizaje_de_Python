15318f61f6871d5af070584af07b3f1a0d93bafd
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-10 22:14:55 -0500

    Mejorando el navbar del proyecto

--- Flask/app-flask/app-article-front/src/app/components/Navbar.tsx
+++ Flask/app-flask/app-article-front/src/app/components/Navbar.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
   return (
     <nav className='bg-indigo-600 text-white p-4'>
-        <div className='container mx-auto flex justify-between items-center'>
-            <h1 className='text-xl font-bold'>Mi Artículo</h1>
-            <ul className='flex space-x-4'>
-                <li>
-                    <a href="/">Inicio</a>
-                </li>
-                <li>
-                    <a href="/">Crear</a>
-                </li>
-            </ul>
-        </div>
+       <div className="container mx-auto flex justify-between items-center">
+            <div className="flex items-center space-x-4">
+                <a href="/" className='text-xl font-bold'>Mi Artículo</a>
+                <div className="relative">
+                    <input 
+                    type="text" 
+                    placeholder="Buscar artículos..."
+                    className="bg-gray-300 text-black rounded-full pl-10 pr-4 py-2 focus:outline-none"
+                    />
+                    <FontAwesomeIcon icon={faSearch} className='text-white absolute left-3 top-3 h-5 w-5'/>
+                </div>
+            </div>
+       </div>
     </nav>
   )
 }
