d98d84363ba3655ad863f8fbf9fc1646a09f4f7d
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-11 20:24:03 -0500

    Modificando y añadiendo nuevas etiquetas para el navbar del proyecto

--- Flask/app-flask/app-article-front/src/app/components/Navbar.tsx
+++ Flask/app-flask/app-article-front/src/app/components/Navbar.tsx
@@ -1,22 +1,41 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faSearch, faPlusCircle, faSignInAlt } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
   return (
     <nav className='bg-indigo-600 text-white p-4'>
        <div className="container mx-auto flex justify-between items-center">
-            <div className="flex items-center space-x-4">
-                <a href="/" className='text-xl font-bold'>Mi Artículo</a>
-                <div className="relative">
-                    <input 
-                    type="text" 
-                    placeholder="Buscar artículos..."
-                    className="bg-gray-300 text-black rounded-full pl-10 pr-4 py-2 focus:outline-none"
-                    />
-                    <FontAwesomeIcon icon={faSearch} className='text-white absolute left-3 top-3 h-5 w-5'/>
-                </div>
-            </div>
+        <a href="/" className='text-xl font-bold'>Mi Artículo</a>
+
+        <div className='flex-grow'>
+          <div className="relative max-w-md mx-auto">
+                  <FontAwesomeIcon icon={faSearch} className='text-white absolute left-3 top-3 h-5 w-5'/>
+                      <input 
+                      type="text" 
+                      placeholder="Buscar artículos..."
+                      className="bg-gray-300 text-black rounded-full pl-10 pr-4 py-2 w-full focus:outline-none"
+                      />  
+              </div>
+        </div>
+
+            <ul className='flex space-x-6 items-center'>
+              <li>
+                <a href="/create" className='flex items-center hover:text-gray-300'>
+                   <FontAwesomeIcon icon={faPlusCircle} className='mr-2 h-6 w-6'/>
+                   Crear Artículo
+                </a>
+              </li>
+
+              <li>
+                <a href="/login" className='flex items-center hover:text-gray-300'>
+                   <FontAwesomeIcon icon={faSignInAlt} className='mr-2 h-6 w-6'/>
+                   Iniciar Sesión
+                </a>
+              </li>
+
+            </ul>
+
        </div>
     </nav>
   )
