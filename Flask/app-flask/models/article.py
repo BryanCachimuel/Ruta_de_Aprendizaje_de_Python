fe8228465f848425f073cf834f2599988fbd28a1
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-11 21:15:50 -0500

    Agregando al modelo el atributo image_url para poder subir imagenes

--- Flask/app-flask/models/article.py
+++ Flask/app-flask/models/article.py
@@ -5,6 +5,7 @@ class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     content = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.String(255))
 
     # se imprime por consola el titulo del artículo
     def __repr__(self):
