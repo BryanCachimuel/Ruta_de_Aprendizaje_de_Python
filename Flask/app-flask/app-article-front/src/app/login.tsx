15cf0da5b603fbba9d8d00f2fd7e98c63d2bb043
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-12 21:37:36 -0500

    Maquetando el formulario para inicio de sesión

--- Flask/app-flask/app-article-front/src/app/login.tsx
+++ Flask/app-flask/app-article-front/src/app/login.tsx
@@ -27,7 +27,39 @@ const login = () =>{
     }
 
     return (
-      <div>login</div>
+      <div className='container mx-auto py-10'>
+        <h1 className="text-3xl font-bold text-center mb-6">Iniciar Sesión</h1>
+       {error && <p className='text-red-500 text-center mb-4f'>{error}</p>}
+
+       <form onSubmit={handleSubmit} className='max-w-md mx-auto'>
+            <div className="mb-4">
+                <label className="block text-gray-700">Email</label>
+                <input 
+                type="email" 
+                className="w-full p-2 border border-gray-300 rounded"  
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                />
+            </div>
+            <div className="mb-4">
+                <label className="block text-gray-700">Password</label>
+                <input 
+                type="password" 
+                className="w-full p-2 border border-gray-300 rounded"  
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                />
+            </div>
+            <button 
+            type='submit' 
+            className='w-full bg-indigo-600 text-white py-2 rounded hover:bg-indigo-500'
+            >
+                Iniciar Sesión
+            </button>
+       </form>
+      </div>
     )
 
 }
