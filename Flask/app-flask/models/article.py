197e300429fb2b5a6c11fe01e1f460bf6d57b7bd
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-14 21:47:46 -0500

    Agregando el atributo created_at para obtener la fecha actual para los artículo

--- Flask/app-flask/models/article.py
+++ Flask/app-flask/models/article.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 # Modelo de Artículo
 from models import db
 
@@ -7,6 +8,7 @@ class Article(db.Model):
     content = db.Column(db.Text, nullable=False)
     image_url = db.Column(db.String(255))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
 
     # se imprime por consola el titulo del artículo
     def __repr__(self):
