c96fff129ea91127fddcfc1dd8fcd5df8ea966d4
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-17 21:35:36 -0500

    Creando un archivo types.ts para colocar las dos interfaces para Article y CardProps las cuales van a ser importadas a sus respectivos componentes

---
+++ Flask/app-flask/app-article-front/src/types/article.ts
@@ -0,0 +1,17 @@
+export interface Article {
+  id: number;
+  title: string;
+  content: string;
+  image_url: string;
+  author: string;
+  created_at: string;
+}
+
+export interface CardProps {
+    id: number;
+    title: string;
+    content: string;
+    image_url: string;
+    author: string;
+    created_at: string
+}
