aa899785be6b95901365ffb7ba1c1efc69667e82
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-17 20:08:14 -0500

    Importando el componente SearchBar hacia la sección de navegación

--- Flask/app-flask/app-article-front/src/app/components/Navbar.tsx
+++ Flask/app-flask/app-article-front/src/app/components/Navbar.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch, faPlusCircle, faSignInAlt } from '@fortawesome/free-solid-svg-icons'
+import { faPlusCircle, faSignInAlt } from '@fortawesome/free-solid-svg-icons'
+import SearchBar from './SearchBar'
 
 const Navbar = () => {
 
@@ -41,16 +42,8 @@ const Navbar = () => {
        <div className="container mx-auto flex justify-between items-center">
         <a href="/" className='text-xl font-bold'>Mi Artículo</a>
 
-        <div className='flex-grow'>
-          <div className="relative max-w-md mx-auto">
-                  <FontAwesomeIcon icon={faSearch} className='text-white absolute left-3 top-3 h-5 w-5'/>
-                      <input 
-                      type="text" 
-                      placeholder="Buscar artículos..."
-                      className="bg-gray-300 text-black rounded-full pl-10 pr-4 py-2 w-full focus:outline-none"
-                      />  
-              </div>
-        </div>
+          {/* Componente para Buscar */}
+          <SearchBar/>
 
             <ul className='flex space-x-6 items-center'>
 
