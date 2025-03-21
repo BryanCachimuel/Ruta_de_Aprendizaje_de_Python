9c508685d543eeaed5982c3613d5ad1a0965c8cc
Bryan_Cachimuel <bryanloyo56@gmail.com>
2025-03-20 20:14:18 -0500

    Mejorando el estilisado de la vista de los artículos por su id

--- Flask/app-flask/app-article-front/src/app/page/article/[id]/page.tsx
+++ Flask/app-flask/app-article-front/src/app/page/article/[id]/page.tsx
@@ -28,7 +28,7 @@ const ArticlePage = ({params}: {params : Promise<{id: string}>}) => {
         return (
             <Layout>
                 <div className="container mx-auto py-10">
-                    <p className="text-center">Cargando Artículo......</p>
+                    <p className="text-center text-gray-500 mt-8">Cargando Artículo......</p>
                 </div>
             </Layout>
         )
@@ -36,9 +36,22 @@ const ArticlePage = ({params}: {params : Promise<{id: string}>}) => {
 
     return (
         <Layout>
-            <div className="container mx-auto py-10">
-                <h1 className="text-4xl font-bold mb-6 text-indigo-600">{article.title}</h1>
-                <p className="text-gray-700">{article.content}</p>
+            <div className="max-w-3xl mx-auto p-6 bg-white rounded-lg shadow-md mt-8">
+                {
+                    article.image_url && (
+                        <div className="mb-6">
+                            <img src={article.image_url} alt={article.title} className="w-full h-64 object-cover rounded-lg shadow-md" />
+                        </div>
+                    )
+                }
+                <h1 className="text-4xl font-bold text-indigo-600 mb-4">{article.title}</h1>
+                <div className="flex items-center text-gray-500 mb-6">
+                    <p className="text-sm">Publicado: {article.created_at}</p>
+                    <span className="mx-2"> | </span>
+                    <p className="text-sm">Autor: {article.author}</p>
+                </div>
+
+                <p className="text-lg text-gray-700 leading-relaxed">{article.content}</p>
             </div>
         </Layout>
     )
