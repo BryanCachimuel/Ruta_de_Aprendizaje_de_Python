0dfa68559954727e7b2e1c779889eced85df8a02
BryanCachimuel <bryanloyo56@gmail.com>
2025-04-03 22:46:47 -0500

    Realizando el ejerciciod e calculo del total a pagar por horas trabajadas

---
+++ Introduccion_Python/Ejercicios/ejercicio4.py
@@ -0,0 +1,6 @@
+horas_trabajadas = int(input('Ingrese la cantidad de horas laboradas =>'))
+costo_hora = float(input('Ingrese el costo por hora =>'))
+
+paga = costo_hora * horas_trabajadas
+
+print(f'Su pago correspondiente es: $ {paga} dólares')
