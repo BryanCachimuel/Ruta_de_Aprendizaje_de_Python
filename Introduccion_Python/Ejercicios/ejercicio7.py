84714e0db92320f9cefbeba0800843df2f66e352
BryanCachimuel <bryanloyo56@gmail.com>
2025-04-03 23:11:39 -0500

    Realizando el ejercicio de operaciones matemáticas para obtener el cociente y el residuo entre la división de dos nùmeros

---
+++ Introduccion_Python/Ejercicios/ejercicio7.py
@@ -0,0 +1,7 @@
+numero1 = float(input("Ingrese un primer número: "))
+numero2 = float(input("Ingrese un segundo número: "))
+
+cociente = round((numero1 / numero2),2)
+reciduo = numero1 % numero2
+
+print(f'La división entre los números {numero1} y {numero2} tiene como resultado como cociente {cociente} y residuo {reciduo}')
