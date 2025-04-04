0cb6add3cdb1f336477036cf78f6b6776a2d5b7d
BryanCachimuel <bryanloyo56@gmail.com>
2025-04-03 23:19:39 -0500

    Realizando el ejercicio para calcular el peso total entre dos productos vendidos

---
+++ Introduccion_Python/Ejercicios/ejercicio8.py
@@ -0,0 +1,6 @@
+robots = int(input('Ingrese el número de robots vendidos: '))
+carros = int(input('Ingrese el número de carros venidos: '))
+
+peso_total = (robots*75) + (carros*112)
+
+print(f'El peso total es: {peso_total}')
