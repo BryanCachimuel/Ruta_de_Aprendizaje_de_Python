10cce1c145c413cfb2c2f937d14b48a7e4fbc809
BryanCachimuel <bryanloyo56@gmail.com>
2025-04-07 22:29:05 -0500

    Validando que el resultado de una división no de como resultado Syntax error

---
+++ Introduccion_Python/Ejercicios/ejercicio12.py
@@ -0,0 +1,7 @@
+numero1 = int(input('Ingrese el primer número: '))
+numero2 = int(input('Ingrese el segundo número: '))
+
+if numero2 == 0:
+    print("Syntax error")
+else:
+    print(f"El resultado es: {numero1/numero2}")
