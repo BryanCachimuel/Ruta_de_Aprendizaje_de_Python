702eda5cbb8be52257c0d18373625ed01dd67c65
BryanCachimuel <bryanloyo56@gmail.com>
2025-06-20 11:56:41 -0500

    Aprendiendo sobre los códigos de estado en el retorno de la función y en el parámetro del decorador de ruta

--- FastApi/main.py
+++ FastApi/main.py
@@ -66,7 +66,7 @@ class MovieCreate(BaseModel):
 # PlainTextResponse -> imprimir una respuesta sin ningun tipo de formato
 @app.get('/', tags=['Home'])
 def home():
-    return PlainTextResponse(content='Hola desde FastAPI')
+    return PlainTextResponse(content='Hola desde FastAPI', status_code=200)
 
 # Formas de uso del método get y sus tipos de respuesta en el retorno que se puede tener
 
@@ -79,11 +79,12 @@ def home():
 
 
 # Obtener el listado de peliculas, -> List[Movie] identifica que está ruta va retornar una lista y de igual forma se muestra en las demás rutas
-@app.get('/movies', tags=['Movies'])
+# El tercer parámetro del decorador de la función indica el códiogo y descripción de estado de la documentación
+# El código de estado del retorno indica el estado de la respuesta
+@app.get('/movies', tags=['Movies'], status_code=200, response_description='Nos debe devolver una respuesta exitosa')
 def get_movies() -> List[Movie]:
-    # return {"movie": "Los Vengadores"}
     content = [movie.model_dump() for movie in movies]
-    return JSONResponse(content=content)
+    return JSONResponse(content=content, status_code=200)
 
 
 # Parámetro en las rutas se agrega en la ruta, en el primer parámetro de la siguiente forma /{id}
@@ -92,16 +93,16 @@ def get_movies() -> List[Movie]:
 def get_movie(id: int = Path(gt=0)) -> Movie | dict:
     for movie in movies:
         if movie.id == id:
-            return JSONResponse(content=movie.model_dump())
-    return JSONResponse(content={})
+            return JSONResponse(content=movie.model_dump(), status_code=200)
+    return JSONResponse(content={}, status_code=404)
 
 # Parámetros query, su estructura en la url se identifica así: localhost:5000/movies/?id=123
 @app.get('/movies/', tags=['Movies'])
 def get_movie_by_category(category: str = Query(min_length=5, max_length=20)) -> Movie | dict:
     for movie in movies:
         if movie.category == category:
-            return JSONResponse(content=movie.model_dump())
-    return JSONResponse(content={})
+            return JSONResponse(content=movie.model_dump(), status_code=200)
+    return JSONResponse(content={}, status_code=404)
 
 
 # Método POST, se agrega el Body para obtener estos resultados desde un formulario
@@ -111,7 +112,7 @@ def get_movie_by_category(category: str = Query(min_length=5, max_length=20)) ->
 def create_movie(movie: MovieCreate) -> List[Movie]:
     movies.append(movie)
     content = [movie.model_dump() for movie in movies]
-    return JSONResponse(content=content)
+    return JSONResponse(content=content, status_code=201)
     #return RedirectResponse('/movies', status_code=303)
     
 
@@ -126,7 +127,7 @@ def update_movie(id: int, movie: MovieUpdate) -> List[Movie]:
             mv.rating = movie.rating
             mv.category = movie.category
     content = [movie.model_dump() for movie in movies]
-    return JSONResponse(content=content)
+    return JSONResponse(content=content, status_code=200)
     
 # Método delete
 @app.delete('/movies/{id}', tags=['Movies'])
@@ -135,7 +136,7 @@ def delete_movie(id: int) -> List[Movie]:
         if movie.id == id:
             movies.remove(movie)
     content = [movie.model_dump() for movie in movies]
-    return JSONResponse(content=content)
+    return JSONResponse(content=content, status_code=200)
 
 
 # Probando el tipo de respuesta de archivo
