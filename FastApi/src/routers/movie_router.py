cc51784f4c286758e7f0061888564bafd1a8d267
BryanCachimuel <bryanloyo56@gmail.com>
2025-06-20 22:54:40 -0500

    Archivo con la edición de las rutas de películas

--- FastApi/src/routers/movie_router.py
+++ FastApi/src/routers/movie_router.py
@@ -1,13 +1,16 @@
-from fastapi import Path, Query
+from fastapi import Path, Query, APIRouter
 from fastapi.responses import JSONResponse
 from typing import List
+from src.models.movie_model import Movie, MovieCreate, MovieUpdate
 
 movies: List[Movie] = []
+
+movie_router = APIRouter()
 
 # Obtener el listado de peliculas, -> List[Movie] identifica que está ruta va retornar una lista y de igual forma se muestra en las demás rutas
 # El tercer parámetro del decorador de la función indica el códiogo y descripción de estado de la documentación
 # El código de estado del retorno indica el estado de la respuesta
-@app.get('/movies', tags=['Movies'], status_code=200, response_description='Nos debe devolver una respuesta exitosa')
+@movie_router.get('/', tags=['Movies'], status_code=200, response_description='Nos debe devolver una respuesta exitosa')
 def get_movies() -> List[Movie]:
     content = [movie.model_dump() for movie in movies]
     return JSONResponse(content=content, status_code=200)
@@ -15,7 +18,7 @@ def get_movies() -> List[Movie]:
 
 # Parámetro en las rutas se agrega en la ruta, en el primer parámetro de la siguiente forma /{id}
 # Validaciones de parámetros
-@app.get('/movies/{id}', tags=['Movies'])
+@movie_router.get('/{id}', tags=['Movies'])
 def get_movie(id: int = Path(gt=0)) -> Movie | dict:
     for movie in movies:
         if movie.id == id:
@@ -23,7 +26,7 @@ def get_movie(id: int = Path(gt=0)) -> Movie | dict:
     return JSONResponse(content={}, status_code=404)
 
 # Parámetros query, su estructura en la url se identifica así: localhost:5000/movies/?id=123
-@app.get('/movies/', tags=['Movies'])
+@movie_router.get('/by_category', tags=['Movies'])
 def get_movie_by_category(category: str = Query(min_length=5, max_length=20)) -> Movie | dict:
     for movie in movies:
         if movie.category == category:
@@ -34,7 +37,7 @@ def get_movie_by_category(category: str = Query(min_length=5, max_length=20)) ->
 # Método POST, se agrega el Body para obtener estos resultados desde un formulario
 # model_dum nos trae el esquema de los atributos de la clase Movie
 # RedirectResponse -> redirige hacia una ruta y se agrega el estado del código
-@app.post('/movies', tags=['Movies'])
+@movie_router.post('/', tags=['Movies'])
 def create_movie(movie: MovieCreate) -> List[Movie]:
     movies.append(movie)
     content = [movie.model_dump() for movie in movies]
@@ -43,7 +46,7 @@ def create_movie(movie: MovieCreate) -> List[Movie]:
     
 
 # Método PUT
-@app.put('/movies/{id}', tags=['Movies'])
+@movie_router.put('/{id}', tags=['Movies'])
 def update_movie(id: int, movie: MovieUpdate) -> List[Movie]:
     for mv in movies:
         if mv.id == id:
@@ -56,7 +59,7 @@ def update_movie(id: int, movie: MovieUpdate) -> List[Movie]:
     return JSONResponse(content=content, status_code=200)
     
 # Método delete
-@app.delete('/movies/{id}', tags=['Movies'])
+@movie_router.delete('/{id}', tags=['Movies'])
 def delete_movie(id: int) -> List[Movie]:
     for movie in movies:
         if movie.id == id:
